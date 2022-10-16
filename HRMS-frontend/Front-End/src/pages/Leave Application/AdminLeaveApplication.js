6ee9ebd3d9adea16bb42ea150e9bb128ecbbc673
tarun <tarun.m@pyther.com>
2022-10-16 09:50:47 +0530

    Leave Application

---
+++ HRMS-frontend/Front-End/src/pages/Leave Application/AdminLeaveApplication.js
@@ -0,0 +1,92 @@
+import { apiservice } from 'apiservice'
+import React, { Component } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { Button, Label } from 'reactstrap'
+
+
+function AdminLeaveApplication(){
+    const [LeaveApplications,setLeaveApplications] = useState([])
+    const [response,setResponse] = useState("pending")
+    useEffect(()=>{
+
+        apiservice.callServiceGet('/leave/').then((res)=>{
+            console.log(res.data.result);
+            setLeaveApplications(res.data.result)
+        })
+
+    },[])
+    
+    return(
+        <>
+                   
+                   <div className="">
+                    <br></br>
+                    <br></br>
+                        <h2> Show applications </h2>
+                        {LeaveApplications.map(leave => {
+                            return (
+                                <div className="leaveCard">
+                                    <div className="leaveDateWrapperHistory">
+                                        <label className="leaveDateLabelHistory" htmlFor="name">Name: </label>
+                                        <p className="leaveDateHistory" name="name">{leave.userId.name}</p>
+                                    </div>
+                                    <div className="leaveDateWrapperHistory">
+                                        <label className="leaveDateLabelHistory" htmlFor="email">Email:</label>
+                                        <p className="leaveDateHistory" name="email">{leave.userId.email}</p>
+                                    </div>
+                                    
+                                    <div className="leaveDateWrapperHistory">
+                                        <label className="leaveDateLabelHistory" htmlFor="leaveDate">Start Date:</label>
+                                        <p className="leaveDateHistory">{leave.startDate.slice(0, 10)}</p>
+                                    </div>
+                                    <div className="leaveDateWrapperHistory">
+                                        <label className="leaveDateLabelHistory" htmlFor="leaveDate">End Date:</label>
+                                        <p className="leaveDateHistory">{leave.endDate.slice(0, 10)}</p>
+                                    </div>
+                                    
+                                    <div className="leaveReasonWrapperHistory">
+                                        <label className="leaveReasonLabelHistory" htmlFor="leaveReason">Reason</label>
+                                        <p className="leaveReasonHistory">{leave.reason}</p>
+                                    </div>
+                                    <div className="mb-3">
+                                   < input id="userEmail" name="response" className="form-control" type="text" list="details" value={response}
+                                                            placeholder="Enter Employee Email..."
+                                                            onChange={(e) => { setResponse(e.target.value) }} />
+
+                                                            <datalist id="details">
+                                                                <option data-value="pending">Pending</option>
+                                                                <option data-value="approved">approved</option>
+                                                                <option data-value="denied">denied</option>
+                                                            </datalist>
+                                                       
+                                {/* <Label className="form-label" htmlFor="status">Response</Label>
+                                 <div className="col-md-10">
+                              <select className="form-control" name='status' onChange={(e)=>{
+                                setResponse(e.target.value)
+                              }}
+                            value={leave.admin_response || "pending"}>
+                            <option value="pending">Pending</option>
+                            <option value="approved">Approved</option>
+                            <option value="denied">Denied</option>
+                            
+                          </select> */}
+                        {/* </div> */}
+                        <button className="leaveBtn" type="submit" onClick={(e)=>{
+                            e.preventDefault();
+                            apiservice.callServicePut('/leave/'+leave._id , {admin_response:response})
+                        }} style={{"margin-bottom":10}}>Update</button>
+                       
+                                </div>
+                                </div>
+
+                            )
+                        })}
+
+                    </div>
+                    
+        
+        </>
+    )
+}
+export default AdminLeaveApplication;
--- HRMS-frontend/Front-End/src/pages/Leave Application/LeaveApplication.js
+++ HRMS-frontend/Front-End/src/pages/Leave Application/LeaveApplication.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "reactstrap"
 import { apiservice } from "service/apiservice";
+import service from "service/constant";
 
 
 
 const LeaveApplication = () => {
 
     const [leaves, setleaves] = useState([])
+    const [startDate,setStartDate] = useState("");
+    const [endDate,setEndDate] = useState("");
+    const [title,setTitle] = useState("");
+    const [reason,setReason] = useState("")
+    
 
     let callApi = async () => {
-        let res = await apiservice.callServiceGet("/leave/")
+        let res = await apiservice.callServiceGet("/leave/"+_id)
         setleaves(res.data.result);
         console.log(res.data.result);
     }
@@ -31,33 +37,61 @@ const LeaveApplication = () => {
         setleaveReason(e.target.value)
     }
 
+    let {_id} = JSON.parse(localStorage.getItem('user'))
 
-
-    let handleSubmit = () => {
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(startDate,endDate,title,reason)
         console.log(leaveDate, leaveReason);
+        apiservice.callServicePostFormdata('/leave/'+_id ,{startDate:startDate,endDate:endDate,title:title,reason:reason} )
         // TODO : API Call for leave Application
     }
 
     return (
         <>
+        
             <div className="page-content">
                 <Container fluid>
                     <h2> Apply for Leave </h2>
+                    
                     <div className="applyLeaveWrapper">
                         <div className="leaveDateWrapper">
-                            <label className="leaveDateLabel" htmlFor="leaveDate">Date</label>
-                            <input className="leaveDate" value={leaveDate} onChange={handleChangeDate} type="date" />
+                            <label className="leaveDateLabel" htmlFor="leaveDate">StartDate</label>
+                            <input className="leaveDate" name="startDate" value={startDate} onChange={(e)=>{
+                                setStartDate(e.target.value)
+                            }} type="date" />
+
+                            <label className="leaveDateLabel" htmlFor="leaveDate">EndDate</label>
+                            <input className="leaveDate" name="endDate" value={endDate} onChange={(e)=>{
+                                setEndDate(e.target.value)
+                            }} type="date" />
                         </div>
                         <div className="leaveReasonWrapper">
-                            <label className="leaveReasonLabel" htmlFor="leaveReason">Reason</label>
-                            <input className="leaveReason" value={leaveReason} onChange={handleChangeReason} type="text" />
+                            <label className="leaveReasonLabel" htmlFor="leaveReason">Title</label>
+                            <input className="leaveReason" name = "title" value={title} onChange={(e)=>{
+                                setTitle(e.target.value)}} type="text" />
                         </div>
-                        <button className="leaveBtn" type="submit" onClick={handleSubmit}>Submit</button>
+                        
+                        
+                        <div className="leaveReasonWrapper">
+                            <label className="leaveReasonLabel" htmlFor="leaveReason">Reason</label>
+                            <input className="leaveReason" name = "reason" value={reason} onChange={(e)=>{
+                                setReason(e.target.value)}} type="text" />
+                        </div>
+                        <button className="leaveBtn" type="submit" onClick={handleSubmit} style={{"margin-bottom":10}}>Submit</button>
+                        <br></br>
                     </div>
+                    </Container>
+            
 
-
-                    <div className="leaveHistory">
-                        <h2> Show applications </h2>
+                    <div>
+                        
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    
+                    <h2> Show My applications </h2>
                         {leaves.map(leave => {
                             return (
                                 <div className="leaveCard">
@@ -72,13 +106,13 @@ const LeaveApplication = () => {
                                     <div className="leaveStatus">
                                         <h5>{leave.admin_response}</h5>
                                     </div>
-                                </div>
-                            )
-                        })}
-
+                                </div>)})}
+                   
                     </div>
-                </Container>
-            </div>
+                    
+                    
+                    
+                </div>
         </>
     )
 }
